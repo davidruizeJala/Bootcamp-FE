a1e389a852e3c7ddea1fa93228a106b20a4f90ed
davidruizeJala <david.ruiz@jala.university>
2026-05-22 13:04:53 -0500

    5 module done

--- angular-tutor-vs/src/app/app.ts
+++ angular-tutor-vs/src/app/app.ts
@@ -12,14 +12,21 @@ import { MOCK_RECIPE_1, MOCK_RECIPE_2 } from './mock-recipes';
 export class App {
   protected readonly title = signal('My Recipe Box');
   protected readonly recipe = signal<RecipeModel>(MOCK_RECIPE_1);
+  protected readonly servings = signal<number>(1);
 
   protected showRecipe1(): void {
-    console.log('Showing recipe 1');
     this.recipe.set(MOCK_RECIPE_1);
   }
 
   protected showRecipe2(): void {
-    console.log('Showing recipe 2');
     this.recipe.set(MOCK_RECIPE_2);
+  }
+
+  protected increaseServings(): void {
+    this.servings.update((current) => current + 1);
+  }
+
+  protected decreaseServings(): void {
+    this.servings.update((current) => Math.max(1, current - 1));
   }
 }
