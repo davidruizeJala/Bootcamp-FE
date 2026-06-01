fb5bfe8cf140f8c87d658d9c23a6d4dfec176445
davidruizeJala <david.ruiz@jala.university>
2026-06-01 12:56:00 -0500

    module 7 finished

--- angular-tutor-vs/src/app/mock-recipes.ts
+++ angular-tutor-vs/src/app/mock-recipes.ts
@@ -11,6 +11,8 @@ export const MOCK_RECIPE_1: RecipeModel = {
     { name: 'Pecorino Romano Cheese', quantity: 50, unit: 'g' },
     { name: 'Black Pepper', quantity: 1, unit: 'tsp' },
   ],
+  imgUrl:
+    'https://pastaslamuneca.com/wp-content/uploads/2021/04/Pasta-carbonara-receta-italiana-original-768x512.png',
 };
 
 export const MOCK_RECIPE_2: RecipeModel = {
@@ -23,4 +25,6 @@ export const MOCK_RECIPE_2: RecipeModel = {
     { name: 'Fresh Basil', quantity: 1, unit: 'bunch' },
     { name: 'Extra Virgin Olive Oil', quantity: 2, unit: 'tbsp' },
   ],
+  imgUrl:
+    'https://www.huleymantel.com/uploads/s1/36/17/27/ensalada-caprese-tomates-maduros-queso-mozzarella-hojas-albahaca-fresca-166116-3714.jpeg',
 };
